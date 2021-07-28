1163f73b785152eb9681c33b88f809db6e77b0b7
le Téno <leteno.loic@gmail.com>
2021-07-28 16:01:50 +0200

    base de données pokemons LLT

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
+
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-
+                <h1>hello</h1>
             </header>
         </div >
     );
---
+++ src/models/pokemons.js
@@ -0,0 +1,20 @@
+class POKEMON extends Component {
+
+    // Valeur de base pokémons
+    constructor(id, hp = 100, cp = 10, name = 'name', picture = 'http://...', types = ['Normal'], created = new Date()) {
+        
+        // Données demander pour créer le pokemon
+        this.id = id;
+        this.hp = hp;
+        this.cp = cp;
+        this.name = name;
+        this.picture = picture;
+        this.types = types;
+        this.created = created;
+    }
+    render() {
+        return;
+    }
+}
+
+export default POKEMON;
